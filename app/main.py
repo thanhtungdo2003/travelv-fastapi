1aba71caebd48229eed1cf8393ce62b420e35d85
thanhtungdo2003 <Tungdohotat12345@gmail.com>
2025-09-23 09:04:41 +0700

    fix: cors

--- app/main.py
+++ app/main.py
@@ -35,6 +35,7 @@ origins = [
     "http://127.0.0.1:4000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://thanhtungdo2003.github.io/travelv-landingpage",
 ]
 
 app.add_middleware(
